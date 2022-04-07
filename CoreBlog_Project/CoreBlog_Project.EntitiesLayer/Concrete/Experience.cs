71ef51422edef14f064a99338b8fa710f880914d
Fırat Alçın <firatalcin04@gmail.com>
2022-04-07 15:23:42 +0300

    Update

---
+++ CoreBlog_Project/CoreBlog_Project.EntitiesLayer/Concrete/Experience.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreBlog_Project.EntityLayer.Concrete
+{
+    public class Experience
+    {
+        [Key]
+        public int ExperienceId { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
