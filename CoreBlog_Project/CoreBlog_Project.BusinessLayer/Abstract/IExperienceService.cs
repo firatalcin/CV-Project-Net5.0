754b979c165693dc874c1edda9b7e33c2a410b0d
Fırat Alçın <firatalcin04@gmail.com>
2022-04-08 18:49:26 +0300

    Update

---
+++ CoreBlog_Project/CoreBlog_Project.BusinessLayer/Abstract/IExperienceService.cs
@@ -0,0 +1,13 @@
+﻿using CoreBlog_Project.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBlog_Project.BusinessLayer.Abstract
+{
+    public interface IExperienceService : IGenericService<Experience>
+    {
+    }
+}
