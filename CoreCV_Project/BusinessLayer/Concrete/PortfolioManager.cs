6450b258ac87ad4b0d4163b0a1b27834b5419542
Fırat Alçın <firatalcin04@gmail.com>
2022-04-10 17:29:28 +0300

    Update

---
+++ CoreCV_Project/BusinessLayer/Concrete/PortfolioManager.cs
@@ -0,0 +1,46 @@
+﻿using CoreCV_Project.BusinessLayer.Abstract;
+using CoreCV_Project.DataAccessLayer.Abstract;
+using CoreCV_Project.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class PortfolioManager : IPortfolioService
+    {
+        IPortfolioDal _portfolioDal;
+
+        public PortfolioManager(IPortfolioDal portfolioDal)
+        {
+            _portfolioDal = portfolioDal;
+        }
+
+        public void TAdd(Portfolio entity)
+        {
+            _portfolioDal.Add(entity);
+        }
+
+        public Portfolio TGetById(int id)
+        {
+            return _portfolioDal.GetById(id);
+        }
+
+        public List<Portfolio> TGetList()
+        {
+            return _portfolioDal.GetList();
+        }
+
+        public void TRemove(Portfolio entity)
+        {
+            _portfolioDal.Delete(entity);
+        }
+
+        public void TUpdate(Portfolio entity)
+        {
+            _portfolioDal.Update(entity);
+        }
+    }
+}
---
+++ CoreCV_Project/CoreCV_ProjectWithMvc/ViewComponents/Portfolio/PortfolioList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using CoreCV_Project.DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCV_ProjectWithMvc.ViewComponents.Portfolio
+{
+    public class PortfolioList : ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
