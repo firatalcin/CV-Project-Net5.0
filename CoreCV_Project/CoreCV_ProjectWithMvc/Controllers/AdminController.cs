159b0053f941346e746fd3cc90c0a37d51c83458
Fırat Alçın <firatalcin04@gmail.com>
2022-04-11 18:01:25 +0300

    Update

---
+++ CoreCV_Project/CoreCV_ProjectWithMvc/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCV_ProjectWithMvc.Controllers
+{
+    public class AdminController : Controller
+    {
+       public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ CoreCV_Project/CoreCV_ProjectWithMvc/Controllers/FeatureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCV_ProjectWithMvc.Controllers
+{
+    public class FeatureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
