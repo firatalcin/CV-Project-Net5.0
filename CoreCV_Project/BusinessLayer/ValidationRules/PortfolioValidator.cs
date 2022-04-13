c96c74834da32b5023d1b208caa0e583300a3100
Fırat Alçın <firatalcin04@gmail.com>
2022-04-13 18:35:44 +0300

    Update

---
+++ CoreCV_Project/BusinessLayer/ValidationRules/PortfolioValidator.cs
@@ -0,0 +1,24 @@
+﻿using CoreCV_Project.EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class PortfolioValidator : AbstractValidator<Portfolio>
+    {
+        public PortfolioValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Bu alan boş geçilemez");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Bu alan boş geçilemez");
+            RuleFor(x => x.ImageUrl2).NotEmpty().WithMessage("Bu alan boş geçilemez");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Bu alan boş geçilemez");
+            RuleFor(x => x.Value).NotEmpty().WithMessage("Bu alan boş geçilemez");
+            RuleFor(x => x.Name).MinimumLength(5).WithMessage("En az 5 karakter kullanılmalıdır");
+            RuleFor(x => x.Name).MaximumLength(100).WithMessage("En fazla 100 karakter kullanılmalıdır.");
+        }
+    }
+}
