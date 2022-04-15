5e5364318801879e969fd8a7de928db9a690df2d
Fırat Alçın <firatalcin04@gmail.com>
2022-04-15 18:01:12 +0300

    Update

---
+++ CoreCV_Project/CoreCV_ProjectWithMvc/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCV_ProjectWithMvc.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.d1 = "Default";
+            ViewBag.d2 = "Dashboard";
+            ViewBag.d3 = "Default";
+            return View();
+        }
+    }
+}
---
+++ CoreCV_Project/CoreCV_ProjectWithMvc/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCV_ProjectWithMvc.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
