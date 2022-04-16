3cde5f5980af98a0e3dac5799e2ccee4535178d3
Fırat Alçın <firatalcin04@gmail.com>
2022-04-16 18:34:24 +0300

    Update

---
+++ CoreCV_Project/CoreCV_ProjectWithMvc/Controllers/Test2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCV_ProjectWithMvc.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
