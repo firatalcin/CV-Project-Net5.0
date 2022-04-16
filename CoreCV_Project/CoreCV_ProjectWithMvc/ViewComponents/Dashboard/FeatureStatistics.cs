8456da6e892ecfacc45da19a521e5eda3ce4f214
Fırat Alçın <firatalcin04@gmail.com>
2022-04-16 16:48:37 +0300

    Update

---
+++ CoreCV_Project/CoreCV_ProjectWithMvc/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCV_ProjectWithMvc.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
