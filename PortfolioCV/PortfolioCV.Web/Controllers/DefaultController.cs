ce21043af5d957039441ac374974c12f561a89a2
Fırat Alçın <firatalcin04@gmail.com>
2022-07-28 01:15:01 +0300

    index Template

---
+++ PortfolioCV/PortfolioCV.Web/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioCV.Web.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
