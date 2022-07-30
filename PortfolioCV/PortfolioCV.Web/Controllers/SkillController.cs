69818bfaf7d11563f587d1f51a9acaf0d1ae6ccb
Fırat Alçın <firatalcin04@gmail.com>
2022-07-30 22:04:09 +0300

    Admin Teması ve Layout

---
+++ PortfolioCV/PortfolioCV.Web/Controllers/SkillController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioCV.Web.Controllers
+{
+    public class SkillController : Controller
+    {
+        private readonly ISkillService _skillService;
+
+        public IActionResult Index()
+        {
+            var list = _skillService.GetAll();
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddSkill(Skill s)
+        {
+            _skillService.Add(s);
+            return RedirectToAction("Index");
+        }
+    }
+}
